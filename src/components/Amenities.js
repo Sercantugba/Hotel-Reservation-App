c31e0b6c5ca9afb862c55d4b91eecdbfc2788856
Sercantugba <sercantugba834@gmail.com>
2020-09-14 16:00:58 -0500

    created amenities file for hotel

---
+++ src/components/Amenities.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import '../CSS/Amenities.css'
+
+export default function Amenities() {
+    return (
+
+        <React.Fragment>
+            <div className="amenities-main-div">
+                <div>
+                    <p>TRUE HOSPITALITY</p>
+                    <p>
+                    <br/>Hand sanitizer provided
+                    <br/>Spa and Sauna
+                    <br/>Daily Housekeeping
+                    <br/>Dry Cleaning Services
+                    <br/>Private GYM
+                    <br/>
+                </p>
+                </div>
+                <div>
+                <p>
+                    <br/>Breakfast
+                    <br/>Bottomless Drinks Every Night
+                    <br/>Parking
+                    <br/>Shuttle to Local Attractions
+                    <br/>WiFi
+                    <br/>Non-Smoking Hotel
+                    <br/>Pet Friendly
+                    <br/>Pool - Outdoor
+                    </p>
+                </div>       
+            </div>
+        </React.Fragment>
+               
+           
+
+            
+     
+    )
+}
