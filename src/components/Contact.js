802d3860d0655a8a854774f2bc200e8ed00cb4ad
Sercantugba <sercantugba834@gmail.com>
2020-09-14 15:56:29 -0500

    created file for contact info

---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+import React from 'react'
+// import '../CSS/Contact.css'
+
+export default function AboutUsContact() {
+    return (
+        <React.Fragment>
+        <div className="amenities-main-div">
+          <h5>
+
+          Sercan Beach Island Resort
+          </h5>  
+          <p>Lose yourself at a place where you can sense a truer,more timeless Florida.Escape to the wild beauty and unrivaled island of this luxury resort with emblematic sea views, crystal clear waters, sparkling infinity pools, a cuisine that celebrates the gastronomic legacy of the region, and personalized services, all on an island brimming with history and culture.</p>
+         
+        </div>
+        <div className="amenities-main-div">
+            <h5>
+            Contact Us|Locations
+            </h5>  
+            <p>
+             Ph: 786-292-0260
+             <br/>
+             Address: 
+             1 Knights Key Boulevard MM, Marathon,FL 33050
+             <br/>
+             Email: sercanbeach.resort@gmail.com
+
+            </p>
+        </div>
+        </React.Fragment>
+    )
+}
