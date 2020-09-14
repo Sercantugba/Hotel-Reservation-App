73c3d77010a8fe2167bc59ef9c73559c5544cd43
Sercantugba <sercantugba834@gmail.com>
2020-09-14 15:57:06 -0500

    created home file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import '../CSS/Home.css'
 
 
 export default function Home() {
     return (
         <div className="home-container">
-            
+            <h1>ENJOY THE SILENCE</h1>
             
         </div>
     )
