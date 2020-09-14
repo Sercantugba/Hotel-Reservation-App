a0439637658615e8f25192c233e2f7c8b0b207e2
Sercantugba <sercantugba834@gmail.com>
2020-09-14 15:54:34 -0500

    created func component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 import Home from './components/Home'
 import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Book from './components/Book'
+import Amenities from './components/Amenities'
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Navbar />
-      <Switch>
-      <Route exact path = '/' component={Home} />
-
-      </Switch>
+      
+      
+     
     </div>
     </Router>
   );
