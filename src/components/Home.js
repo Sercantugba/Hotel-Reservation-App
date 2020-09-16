d658be5962e590d25fd69110a1161806cdd4102d
Sercantugba <sercantugba834@gmail.com>
2020-09-15 21:24:38 -0500

    cont. working on home file

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,8 @@ export default function Home() {
         <div className="home-container">
             {/* <img src={require("../images/hotel.jpg")}/> */}
             <h1>ANDROS ISLAND RESORT</h1>
+            {/* <br/>
+            <p>Enjoy The Silence</p> */}
 
             
         </div>
