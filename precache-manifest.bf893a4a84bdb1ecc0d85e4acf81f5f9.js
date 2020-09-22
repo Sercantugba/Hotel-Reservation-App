930cc107433edd1c789f4dbc459e8579db018018
Sercantugba <sercantugba834@gmail.com>
2020-09-22 17:32:31 -0500

    Updates

---
+++ precache-manifest.bf893a4a84bdb1ecc0d85e4acf81f5f9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f219f41ac0432f26f7bca473d8fa214b",
+    "url": "/Hotel-Reservation-App/index.html"
+  },
+  {
+    "revision": "769a36535ffb86a2daa3",
+    "url": "/Hotel-Reservation-App/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "62a4e67bbc197cbf99e1",
+    "url": "/Hotel-Reservation-App/static/css/main.a62136ca.chunk.css"
+  },
+  {
+    "revision": "769a36535ffb86a2daa3",
+    "url": "/Hotel-Reservation-App/static/js/2.6c892679.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/Hotel-Reservation-App/static/js/2.6c892679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a4e67bbc197cbf99e1",
+    "url": "/Hotel-Reservation-App/static/js/main.08a9f530.chunk.js"
+  },
+  {
+    "revision": "a2042f16c9a7541b6e1c",
+    "url": "/Hotel-Reservation-App/static/js/runtime-main.e4634a7b.js"
+  },
+  {
+    "revision": "fb7644429a2c95b0e3d32b2af723c9a5",
+    "url": "/Hotel-Reservation-App/static/media/hotel-room-.fb764442.jpg"
+  },
+  {
+    "revision": "689eec7d1a9bf2b1c94971870741eb5f",
+    "url": "/Hotel-Reservation-App/static/media/hotel.689eec7d.jpg"
+  },
+  {
+    "revision": "6a3495c0ff6f77e53ac0b159491bc2f4",
+    "url": "/Hotel-Reservation-App/static/media/pool.6a3495c0.jpg"
+  },
+  {
+    "revision": "7fbba1395dd593701ee94550706a3d69",
+    "url": "/Hotel-Reservation-App/static/media/pool2.7fbba139.jpg"
+  },
+  {
+    "revision": "7b308c3126540f611dfc5038e6663cef",
+    "url": "/Hotel-Reservation-App/static/media/rest.7b308c31.jpg"
+  },
+  {
+    "revision": "85842331a521d77c216a75e9f7e4bfba",
+    "url": "/Hotel-Reservation-App/static/media/room.85842331.jpg"
+  },
+  {
+    "revision": "d8eb8d3519b785d792a1eaa4b4322b34",
+    "url": "/Hotel-Reservation-App/static/media/sea.d8eb8d35.jpg"
+  },
+  {
+    "revision": "b8c7099a10363c3b69889c4a9617a93a",
+    "url": "/Hotel-Reservation-App/static/media/suite.b8c7099a.jpg"
+  },
+  {
+    "revision": "3501d570d9cd27f377e40d553502edb1",
+    "url": "/Hotel-Reservation-App/static/media/type2.3501d570.jpg"
+  },
+  {
+    "revision": "3228d250e6016d73122b8734dc6dab71",
+    "url": "/Hotel-Reservation-App/static/media/type_hotel.3228d250.jpg"
+  },
+  {
+    "revision": "0a66f48da9a88196a55ba3df85103cf7",
+    "url": "/Hotel-Reservation-App/static/media/typethree.0a66f48d.jpg"
+  }
+]);
